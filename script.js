d5ab08d7fa572ee0de0b1a88c3f2be432e41a276
0o01182550 <163905676+0o01182550@users.noreply.github.com>
2024-09-10 04:39:17 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,244 @@
+var product = [{
+    id: 1,
+    img: 'https://static.grainger.com/rp/s/is/image/Grainger/2ETP8_AW01',
+    name: 'Concrete Saws',
+    price: 7000,
+    description: 'เครื่องตัดคอนกรีต จะตัดวัสดุได้สองวิธี วิธีเปียกและแห้ง ความแตกต่างระหว่างสองอย่างนี้คือการใช้น้ำแบบเปียก เพื่อป้องกันไม่ให้ใบเลื่อยร้อนเกินไปขณะตัด ใบเลื่อยกลมหมุนรอบแกนนอนด้วยความเร็ว 1200 RPM ขึ้นไป เมื่อใช้อุปกรณ์นี้ การกำจัดอนุภาคฝุ่นจะลดลงระหว่างขั้นตอนการตัด จึงทำให้งานเสร็จภายในระยะเวลาอันสั้น การทำงานที่ไร้แรงเสียดทาน จะช่วยปรับแต่งผิวสำเร็จของวัสดุที่ตัด ด้วยความแม่นยำสูงสุด',
+    type: 'Saws'
+}, {
+    id: 2,
+    img: 'https://assets.leevalley.com/Size4/10108/13T0121-21-inch-buck-saw-f-0404.jpg',
+    name: 'Hand Saws',
+    price: 1500,
+    description: 'เลื่อยมือที่หลายท่านน่าจะรู้จักกันนี้เลื่อยชนิดนี้ใช้สำหรับตัดไม้ โดยสามารถเลือกขนาดซี่ฟันให้เหมาะกับงานไม้ต่างๆ ได้ดังนี้ งานตัดไม้ในแนวขวางที่ต้องการความละเอียดเพื่อให้หน้าตัดมีความเรียบควรเลือกซี่ฟัน 10-12 ซี่ต่อ 1 นิ้วซึ่งเป็นขนาดฟันเลื่อยที่ค่อนข้างถี่ และหากต้องการตัดไม้ในแนวยาวที่ต้องการความรวดเร็วในการตัดไม่เน้นงานปราณีต สามารถเลือกซี่ฟันที่ค่อนข้างห่าง ประมาณ 5-6 ซี่ต่อ 1 นิ้ว',
+    type: 'Saws'
+}, {
+    id: 3,
+    img: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTC8apy-FsbiCvtmDGSlgtgPIMFHaYuSazl_Q&s',
+    name: 'Bosch GBH 2-26 DE',
+    price: 45000,
+    description: 'Bosch รุ่น GBH 2-26 DE คือ สว่านโรตารี่สำหรับเจาะกระแทกคอนกรีตโดยเฉพาะ เหมาะกับงานฝ้าเพดาน, ฝาผนัง และพื้น กำลังไฟ 800 วัตต์ ความเร็ว 0 – 900 รอบ/นาที อัตรากระแทก 0 – 4,000 ครั้ง/นาที แรงกระแทก 3 จูล มีอัตราการเจาะเหนือกว่าสว่านกระแทกทั่วไป อีกทั้งยังมาพร้อมระบบหยุดกระแทกเพื่อนำไปเจาะไม้และเหล็กกล้า และระบบสกัด ที่ปรับดอกสว่านให้หยุดหมุนเพื่อนำไปกระเทาะวัสดุต่าง ๆ เช่น พื้น, ผนังคอนกรีต และ ปูนฉาบเรียบ มีความแข็งแรงทนทาน ได้คุณภาพ ตามมาตรฐาน Bosch',
+    type: 'Drills'
+},{
+    id: 4,
+    img: 'https://assets.central.co.th//adobe/dynamicmedia/deliver/dm-aid--bf7a3a0d-cc44-47fb-b7c2-b1410a22735b/milwaukee-m12fddxkit-0xinstallationdrilldriverwith20ahbatteryandchargerred-black-mkp1425241-2.jpg?preferwebp=true&quality=60&width=550',
+    name: 'Milwaukee M12 FDDXKIT-0X',
+    price: 2300,
+    description: 'สว่านพร้อมหัวเจาะ 4 แบบ ขนาดกะทัดรัดประสิทธิภาพสูง เพื่อความเหมาะสมกับการทำงานที่แตกต่างกัน ระบบหัวจับดอกสว่านแบบถอดออกได้ที่มีหัวที่แตกต่างกัน 4 หัวสำหรับการใช้งานที่หลากหลายเพิ่มความคล่องตัวในการทำงานสว่านไขควงหัวหกเหลี่ยมขนาด 1/4″ ลดความยาวตัวเครื่องเหลือเพียง 130 มม. ใช้งานในที่แคบได้สะดวก มีแถบแม่เหล็กด้านหน้า เพื่อใช้ดูดติดกับดอกสว่านได้อย่างง่าย ชุดแบตเตอรี่ REDLITHIUM™-ION มีโครงสร้างชุดแบตเตอรี่และชิ้นส่วนอิเล็กทรอนิกส์ที่ยอดเยี่ยม และให้การทำงานโดยที่กำลังไม่ตก เพื่อให้ระยะเวลาใช้งานที่นานขึ้นตลอดอายุการใช้งานของชุดแบตเตอรี่',
+    type: 'Drills'
+},{
+    id: 5,
+    img: 'https://th.bing.com/th/id/OIP.SddEKjWSJnGqdNLek6o5ZwHaHa?rs=1&pid=ImgDetMain',
+    name: 'Knipex VDE 200mm',
+    price: 1600,
+    description: '(Snipe nose pliers straight 1000VDE 200mm) ปลายยืดหยุ่น: รูปร่างมั่นคงแม้ว่าจะถูกบิดปลายที่ทนการบิดที่ยืดหยุ่นอย่างแม่นยำ ก้ามหนีบครี่งวงกลม ยาว แหลมมาพร้อมกับขอบตัดสำหรับสายไฟหรือลวดชนิดอ่อน, แข็งปานกลางและแข็ง',
+    type: 'Snipenose'
+},{
+    id: 6,
+    img: 'https://th.bing.com/th/id/OIP.oM4zO9IQ8a8bYoUaNYQnLgHaHa?rs=1&pid=ImgDetMain',
+    name: 'GCO 230',
+    price: 5000,
+    description: 'ทำการตัดได้รวดเร็วและราบรื่นยิ่งขึ้นด้วยมอเตอร์ 2,300 วัตต์แบบใหม่ ช่วยให้คุณทำงานอย่างปลอดภัยด้วยฝาครอบป้องกันสะเก็ดแบบปรับได้ รองรับการทำงานเกินกำลังได้สูงยิ่งขึ้นเพื่อประสิทธิภาพในการทำงานเกินกำลังถึงขีดสุด เพิ่มประสิทธิภาพในการปกป้องด้วยฝาครอบป้องกันสะเก็ดแบบปรับได้ พกพาและใช้งานได้ง่ายด้วยมือจับที่ออกแบบตามหลักสรีรศาสตร์ซึ่งใช้งานได้ทั้งผู้ที่ถนัดมือซ้ายและมือขวา ฐานรองตัดโลหะป้องกันการละลายขณะตัด',
+    type: 'Saws'
+},{
+    id: 8,
+    img: 'https://somboonkl.co.th/data/media/2021/03/2401501999005.jpg',
+    name: 'Claw Hammer',
+    price: 1600,
+    description: 'ด้ามหยาบ กันลื่น กระชับมือ ดูดซับเหงื่อได้ดีหัวค้อนใช้เหล็กเกรดพิเศษ มีความแข็งแรงทนทานน้ำหนักเบา ช่วยลดแรงขณะใช้งานสามารถใช้ถอนตะปูได้โดยไม่ทำให้ตัวตะปูเสียหายหรือบิดเบี้ยว',
+    type: 'ClawHammer'
+},{
+    id: 9,
+    img: 'https://www.skilthailand.com/wp-content/uploads/2022/12/TS6307SE00-18.png',
+    name: 'TABLE SAW',
+    price: 500,
+    description: 'โต๊ะเลื่อยวงเดือน กำลังไฟ 2,000 วัตต์ ขนาด 10 นิ้วความแม่นยำที่เหนือชั้น เพื่อการตัดที่ที่รวดเร็ว ราบรื่นอย่างมีประสิทธิภาพ ขาตั้งแบบพับได้ในตัวเพื่อการพกพาสะดวกและจัดเก็บง่ายในชุดประกอบด้วย : SKIL โต๊ะเลื่อยวงเดือน กำลังไฟ 2,000 วัตต์ ขนาด 10 นิ้ว',
+    type: 'Saws'
+},{
+    id: 10,
+    img: 'https://content.misumi-ec.com/image/upload/f_webp,q_auto,t_product_main/v1/p/jp/product/series/223000769986/223000769986_001.jpg?$product_main$',
+    name: 'MCC CORPORATION',
+    price: 350,
+    description: 'ทำการตัดได้รวดเร็วและราบรื่นยิ่งขึ้นด้วยมอเตอร์ 2,300 วัตต์แบบใหม่ ช่วยให้คุณทำงานอย่างปลอดภัยด้วยฝาครอบป้องกันสะเก็ดแบบปรับได้ รองรับการทำงานเกินกำลังได้สูงยิ่งขึ้นเพื่อประสิทธิภาพในการทำงานเกินกำลังถึงขีดสุด เพิ่มประสิทธิภาพในการปกป้องด้วยฝาครอบป้องกันสะเก็ดแบบปรับได้ พกพาและใช้งานได้ง่ายด้วยมือจับที่ออกแบบตามหลักสรีรศาสตร์ซึ่งใช้งานได้ทั้งผู้ที่ถนัดมือซ้ายและมือขวา ฐานรองตัดโลหะป้องกันการละลายขณะตัด',
+    type: 'Sliding wrench'
+}];
+
+
+$(document).ready(() => {
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                    <img class="product-img" src="${product[i].img}" alt="">
+                    <p style="font-size: 1.2vw;">${product[i].name}</p>
+                    <p stlye="font-size: 1vw;">${ numberWithCommas(product[i].price) } THB</p>
+                </div>`;
+    }
+    $("#productlist").html(html);
+
+})
+
+function numberWithCommas(x) {
+    x = x.toString();
+    var pattern = /(-?\d+)(\d{3})/;
+    while (pattern.test(x))
+        x = x.replace(pattern, "$1,$2");
+    return x;
+}
+
+function searchsomething(elem) {
+
+    var value = $('#'+elem.id).val()
+    console.log(value)
+
+    var html = '';
+    for (let i = 0; i < product.length; i++) {
+        if( product[i].name.includes(value) ) {
+            html += `<div onclick="openProductDetail(${i})" class="product-items ${product[i].type}">
+                    <img class="product-img" src="${product[i].img}" alt="">
+                    <p style="font-size: 1.2vw;">${product[i].name}</p>
+                    <p stlye="font-size: 1vw;">${ numberWithCommas(product[i].price) } THB</p>
+                </div>`;
+        }
+    }
+    if(html == '') {
+        $("#productlist").html(`<p>Not found product</p>`);
+    } else {
+        $("#productlist").html(html);
+    }
+
+}
+
+function searchproduct(param) {
+    console.log(param)
+    $(".product-items").css('display', 'none')
+    if(param == 'all') {
+        $(".product-items").css('display', 'block')
+    }
+    else {
+        $("."+param).css('display', 'block')
+    }
+}
+
+var productindex = 0;
+function openProductDetail(index) {
+    productindex = index;
+    console.log(productindex)
+    $("#modalDesc").css('display', 'flex')
+    $("#mdd-img").attr('src', product[index].img);
+    $("#mdd-name").text(product[index].name)
+    $("#mdd-price").text( numberWithCommas(product[index].price) + ' THB')
+    $("#mdd-desc").text(product[index].description)
+    $("#mdd-line").text(product[index].link)
+}
+
+function closeModal() {
+    $(".modal").css('display','none')
+}
+
+var cart = [];
+function addtocart() {
+    var pass = true;
+
+    for (let i = 0; i < cart.length; i++) {
+        if( productindex == cart[i].index ) {
+            console.log('found same product')
+            cart[i].count++;
+            pass = false;
+        }
+    }
+
+    if(pass) {
+        var obj = {
+            index: productindex,
+            id: product[productindex].id,
+            name: product[productindex].name,
+            price: product[productindex].price,
+            img: product[productindex].img,
+            link:product[productindex].img,
+            count: 1
+        };
+       
+        cart.push(obj)
+    }
+    console.log(cart)
+
+    Swal.fire({
+        icon: 'success',
+        title: 'Add ' + product[productindex].name + ' to cart !'
+    })
+    $("#cartcount").css('display','flex').text(cart.length)
+}
+
+function openCart() {
+    $('#modalCart').css('display','flex')
+    rendercart();
+}
+
+function rendercart() {
+    if(cart.length > 0) {
+        var html = '';
+        for (let i = 0; i < cart.length; i++) {
+            html += `<div class="cartlist-items">
+                        <div class="cartlist-left">
+                            <img src="${cart[i].img}" alt="">
+                            <div class="cartlist-detail">
+                                <p style="font-size: 1.5vw;">${cart[i].name}</p>
+                                <p style="font-size: 1.2vw;">${ numberWithCommas(cart[i].price * cart[i].count) } THB</p>
+                            </div>
+                        </div>
+                        <div class="cartlist-right">
+                            <p onclick="deinitems('-', ${i})" class="btnc">-</p>
+                            <p id="countitems${i}" style="margin: 0 20px;">${cart[i].count}</p>
+                            <p onclick="deinitems('+', ${i})" class="btnc">+</p>
+                        </div>
+                    </div>`;
+        }
+        $("#mycart").html(html)
+    }
+    else {
+        $("#mycart").html(`<p>Not found product list</p>`)
+    }
+}
+
+function deinitems(action, index) {
+    if(action == '-') {
+        if(cart[index].count > 0) {
+            cart[index].count--;
+            $("#countitems"+index).text(cart[index].count)
+
+            if(cart[index].count <= 0) {
+                Swal.fire({
+                    icon: 'warning',
+                    title: 'Are you sure to delete?',
+                    showConfirmButton: true,
+                    showCancelButton: true,
+                    confirmButtonText: 'Delete',
+                    cancelButtonText: 'Cancel'
+                }).then((res) => {
+                  if(res.isConfirmed) {
+                     cart.splice(index, 1) 
+                     console.log(cart)
+                     rendercart();
+                     $("#cartcount").css('display','flex').text(cart.length)
+                     
+                     if(cart.length <= 0) {
+                        $("#cartcount").css('display','none')
+                     }
+                  }  
+                  else {
+                    cart[index].count++;
+                    $("#countitems"+index).text(cart[index].count)
+                    rendercart();
+                  }
+                })
+            }
+            rendercart();
+        }
+        
+    }
+    else if(action == '+') {
+        cart[index].count++;
+        $("#countitems"+index).text(cart[index].count)
+        rendercart();
+    }
+}
